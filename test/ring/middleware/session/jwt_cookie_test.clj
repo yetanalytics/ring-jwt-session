8e2ae493270668cbf0c0c47574c8ac9f9fec697d
Milt Reder <milt@anestuary.com>
2016-06-10 11:41:40 -0400

    deleted test template, added tests for cookie store

---
+++ test/ring/middleware/session/jwt_cookie_test.clj
@@ -0,0 +1,59 @@
+(ns ring.middleware.session.jwt-cookie-test
+  (:require [clojure.test :refer :all]
+            [ring.middleware.session.jwt-cookie :refer :all]
+            [ring.middleware.session.store :refer [SessionStore
+                                                   read-session
+                                                   write-session
+                                                   delete-session]]
+            [buddy.sign.jwt :as jwt])
+  (:import [ring.middleware.session.jwt_cookie JWTCookieStore]))
+
+(deftest cookie-store-test
+  (let [store (jwt-cookie-store "seekrit")
+        valid-key (jwt/sign {:foo "bar"} "seekrit")]
+
+    (testing "jwt-cookie-store returns a new cookie store"
+      (is (instance? JWTCookieStore store))
+      (testing "that is a ring SessionStore"
+        (is (satisfies? SessionStore store))))
+
+    (testing "read-session"
+      (testing "decodes valid session keys"
+        (is (= {:foo "bar"}
+               (read-session store valid-key))))
+
+      (testing "throws an error for invalid keys"
+        (testing "with a bad signing key"
+          (try
+            (read-session store (jwt/sign {:foo "bar"} "someotherseekrit"))
+            (catch Exception ex
+              (let [{:keys [type cause]} (ex-data ex)]
+                (is (= type :ring.middleware.session.jwt-cookie/invalid-token))
+                (is (= cause {:type :validation :cause :signature}))))))
+
+        (testing "with a malformed key"
+          (try
+            (read-session store (subs valid-key 3))
+            (catch Exception ex
+              (let [{:keys [type cause]} (ex-data ex)]
+                (is (= type :ring.middleware.session.jwt-cookie/invalid-token))
+                (is (= cause {:type :validation :cause :signature}))))))
+        (testing "with a passed :exp key"
+          (try
+            (read-session store (jwt/sign {:foo "bar"
+                                           ;; expires NOW!
+                                           :exp (java.util.Date.)}
+                                          "seekrit"))
+            (catch Exception ex
+              (let [{:keys [type cause]} (ex-data ex)]
+                (is (= type :ring.middleware.session.jwt-cookie/invalid-token))
+                (is (= cause {:type :validation :cause :exp})))))))
+
+      (testing "returns nil for nil keys"
+        (is (nil? (read-session store nil)))))
+    (testing "write-session returns a signed token"
+      (is (= valid-key
+             (write-session store nil {:foo "bar"}))))
+    (testing "delete-session returns a signed token with an empty session"
+      (is (= {}
+             (jwt/unsign (delete-session store nil) "seekrit"))))))
--- test/ring_jwt_session/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns ring-jwt-session.core-test
-  (:require [clojure.test :refer :all]
-            [ring-jwt-session.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))

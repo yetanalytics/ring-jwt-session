b7653a361bfc22858468adeb49435321870d1328
Milt Reder <milt@anestuary.com>
2016-06-09 21:41:56 -0400

    util fn to read the session payload

--- src/cljs/ring_jwt_session/core.cljs
+++ src/cljs/ring_jwt_session/core.cljs
@@ -5,12 +5,17 @@
 (defn- parse-json [s]
   (.parse js/JSON s))
 
+(defn- get-cookie [k]
+  (.get goog.net.cookies k))
+
 (defn get-session
   "Get the ring session payload."
   [& [?key]]
   (some-> (or ?key
               "ring-session")
+          get-cookie
           (str/split #"\.")
           (get 1)
+          js/atob
           parse-json
           (js->clj :keywordize-keys true)))
